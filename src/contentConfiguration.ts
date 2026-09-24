e5822c7f3801e2de04495b887bda42742b9aa8e1
Georgios <giwrgos.flour@gmail.com>
2026-09-25 00:09:39 +0200

    feat: add home page - navigation needs to be decided

---
+++ src/contentConfiguration.ts
@@ -0,0 +1,5 @@
+export const CONTENT_CONFIG = {
+  department:
+    "Institute for Geometry and Applied Mathamatics, RWTH Aachen University",
+  title: "Postdoctoral researcher in Numerical Mathematics",
+};
