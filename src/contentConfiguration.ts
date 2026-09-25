e02249f0695a8408826cbe5c27e60c4021e5584c
Georgios <giwrgos.flour@gmail.com>
2026-09-25 20:21:16 +0200

    feat: add config for the variable texts

--- src/contentConfiguration.ts
+++ src/contentConfiguration.ts
@@ -2,4 +2,7 @@ export const CONTENT_CONFIG = {
   department:
     "Institute for Geometry and Applied Mathamatics, RWTH Aachen University",
   title: "Postdoctoral researcher in Numerical Mathematics",
+  officeAddress:
+    "Office West, Room 403, RWTH Aachen, Im Süsterfeld 2, 52072 Aachen",
+  email: "lastname@igpm.rwth-aachen.de",
 };
